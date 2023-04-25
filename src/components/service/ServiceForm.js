001f84df1e71b3a7b6eea68a0f684d065eb59334
Danilo <daniloadscavalcante@gmail.com>
2023-04-25 20:14:36 -0300

    projeto costs react

---
+++ src/components/service/ServiceForm.js
@@ -0,0 +1,45 @@
+import {useState} from 'react'
+
+import styles from '../project/ProjectFrom.module.css';
+import Input from '../form/Input'
+import SubmitButton from '../form/SubmitButton';
+
+function ServiceForm({handleSubmit, btnText, projectData}){
+const [service, setService] = useState({})
+    function submit(e){
+        e.preventDefault()
+        projectData.services.push(service)
+        handleSubmit(projectData)
+    }
+
+    function handleChange(e){
+        setService({...service, [e.target.name]:e.target.value})
+    }
+return(
+    <form onSubmit={submit} className={styles.form}>
+        <Input 
+        type="text"
+        text="Nome do serviço"
+        name="name"
+        placeholder="Insira o nome do serviço"
+        handleOnchange={handleChange}
+        />
+        <Input 
+        type="number"
+        text="Custo do serviço"
+        name="cost"
+        placeholder="Insira o valor total"
+        handleOnchange={handleChange}
+        />
+        <Input 
+        type="text"
+        text="Descrição do serviço"
+        name="description"
+        placeholder="Descreva o serviço"
+        handleOnchange={handleChange}
+        />
+        <SubmitButton text={btnText} />
+    </form>
+)
+}
+export default ServiceForm
